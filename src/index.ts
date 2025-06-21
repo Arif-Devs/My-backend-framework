18dce8dd4f7bc0e0e726e6ca9f157bf419ecd345
Arifur Rahman <arifur.sew@gmail.com>
2025-06-21 14:06:27 +0600

    server creation stage

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
-import { createServer } from "node:http";
+import { createServer, IncomingMessage, ServerResponse } from "node:http";
 const PORT = 4000;
 
-const server = createServer();
+const requestListener = (req: IncomingMessage, res: ServerResponse) => {
+  const path = req.url;
+  const method = req.method;
+
+  console.log(`${method} ${path}`);
+
+  const response = {
+    statusCode: 200,
+    path: path,
+    method: method,
+  };
+
+  res.writeHead(200, { "Content-Type": "Application/json" });
+  res.write(JSON.stringify(response));
+  res.end();
+};
+
+const server = createServer(requestListener);
+
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
 });
